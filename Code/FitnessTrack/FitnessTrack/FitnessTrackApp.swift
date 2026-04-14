edd6a0158f7c5465827d52c3a776276353520a70
leosbellgalvez <143352389+leosbellgalvez@users.noreply.github.com>
2026-04-14 18:42:32 -0400

    Update FitnessTrackApp.swift

--- Code/FitnessTrack/FitnessTrack/FitnessTrackApp.swift
+++ Code/FitnessTrack/FitnessTrack/FitnessTrackApp.swift
@@ -7,8 +7,8 @@ struct FitnessTrackApp: App {
     init () {
         
         var configuration = ParseConfiguration(
-            applicationId: "0kLOJWaEeKAUoIp9R855YCmniXOdoiRkFGLx81lj",
-            clientKey: "LeShc4ZXDV0FG2UCijfBSLQ82pbrfzg6Pa9zhXiS",
+            applicationId: "INSERT APPLICATION ID HERE",
+            clientKey: "INSERT CLIENT KEY HERE",
             serverURL: URL(string:"https://parseapi.back4app.com")!
         )
         
